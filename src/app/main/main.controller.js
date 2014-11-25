1d00ba48cb4c85c821eae5803f8b1351b08cbc3e
matt-newell <matt.newell@codescience.com>
2014-11-25 17:27:27 -0500

    First try at dynamic images

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,69 +2,81 @@
 
 angular.module('csimages')
   .controller('MainCtrl', function ($scope) {
+    $scope.width = 200;
+    $scope.height = 300;
+
     $scope.awesomeThings = [
       {
         'key': 'angular',
         'title': 'AngularJS',
         'url': 'https://angularjs.org/',
         'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
+        'logo': 'angular.png',
+        'image': 'http://csimages.imgix.net/aloha-turkey.gif'
       },
       {
         'key': 'browsersync',
         'title': 'BrowserSync',
         'url': 'http://browsersync.io/',
         'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
+        'logo': 'browsersync.png',
+        'image': 'http://csimages.imgix.net/benny.gif'
       },
       {
         'key': 'gulp',
         'title': 'GulpJS',
         'url': 'http://gulpjs.com/',
         'description': 'The streaming build system.',
-        'logo': 'gulp.png'
+        'logo': 'gulp.png',
+        'image': 'http://csimages.imgix.net/cellar-heat.gif'
       },
       {
         'key': 'jasmine',
         'title': 'Jasmine',
         'url': 'http://jasmine.github.io/',
         'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
+        'logo': 'jasmine.png',
+        'image': 'http://csimages.imgix.net/indianminion.png'
       },
       {
         'key': 'karma',
         'title': 'Karma',
         'url': 'http://karma-runner.github.io/',
         'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
+        'logo': 'karma.png',
+        'image': 'http://csimages.imgix.net/minion.png'
       },
       {
         'key': 'protractor',
         'title': 'Protractor',
         'url': 'https://github.com/angular/protractor',
         'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
+        'logo': 'protractor.png',
+        'image': 'http://csimages.imgix.net/simple-paisley.gif'
       },
       {
         'key': 'jquery',
         'title': 'jQuery',
         'url': 'http://jquery.com/',
         'description': 'jQuery is a fast, small, and feature-rich JavaScript library.',
-        'logo': 'jquery.jpg'
+        'logo': 'jquery.jpg',
+        'image': 'http://csimages.imgix.net/tuftemap.gif'
       },
       {
         'key': 'bootstrap',
         'title': 'Bootstrap',
         'url': 'http://getbootstrap.com/',
         'description': 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.',
-        'logo': 'bootstrap.png'
+        'logo': 'bootstrap.png',
+        'image': 'http://csimages.imgix.net/typewriter.jpg'
       },
       {
         'key': 'less',
         'title': 'Less',
         'url': 'http://lesscss.org/',
         'description': 'Less extends the CSS language, adding features that allow variables, mixins, functions and many other techniques.',
-        'logo': 'less.png'
+        'logo': 'less.png',
+        'image': 'http://csimages.imgix.net/wiki.png'
       }
     ];
     angular.forEach($scope.awesomeThings, function(awesomeThing) {
